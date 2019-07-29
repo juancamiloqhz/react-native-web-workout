eac535c26df3e71f35dbd84f62404209d487437e
JuanCamiloQHz <juancamiloqhz@gmail.com>
2019-07-29 12:05:38 -0500

    install rimraf, wml (pc) to create a copy of @pyro/common in app. It should replicate when the watchman is watching

--- packages/app/App.tsx
+++ packages/app/App.tsx
@@ -1,23 +1,2 @@
-import React from "react";
-import { StyleSheet, Text, View, Button } from "react-native";
-
-const App = () => {
-  const [count, setCount] = React.useState(0);
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsxdsadf to start working on your app!</Text>
-      <Button title="Add" onPress={() => setCount(d => d + 1)} />
-      <Text>{count}</Text>
-    </View>
-  );
-};
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center"
-  }
-});
+import { App } from "@pyro/common";
 export default App;
